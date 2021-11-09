dad76057bb85327ca1945d58d1fcdc89b767e82c
mmasuda <mmasuda@student.42tokyo.jp>
2021-11-09 23:56:50 +0900

    done for now

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
-chrome.contextMenus.create(
-	createProperties: Object,
-	callback?: function,
-)
-// selectionText
+chrome.contextMenus.create({
+	id: 'search',
+	title: "Search '%s' in the Cambridge Dictionary",
+	contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	if ('search' === info.menuItemId) {
+		var newURL = "https://dictionary.cambridge.org/search/direct/?datasetsearch=english&q=info.selectionText"
+		chrome.tabs.create({url: newURL})
+	}
+});
